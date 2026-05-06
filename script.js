7a6763b76f1c3e8d286a74fabef3a97bac20831d
Aura B+ <bonsualexander16@gmail.com>
2026-05-06 18:00:00 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-let imgs = document.querySelectorAll("img") ;
-
-imgs.forEach(img =>{
-    img.addEventListener("click" , ()=>{
-        window.open(img.src) ;
-    }) ;
-}) ;
+            const imgs = document.querySelectorAll("img") ;
+            let v = document.getElementById("viewer") ;
+            let closeB = document.getElementById("close") ;
+            let v1 = document.getElementById("imgC");
+            let down = document.getElementById("download") ;
+            
+            imgs.forEach(img=>{
+                img.addEventListener("click" , ()=>{
+                    v1.src = img.src
+                    down.download = img.src ;
+                v.classList.toggle("active") ;
+                })
+                
+                v1.addEventListener("click", (e) => {
+                e.stopPropagation();
+       });
+            })
+            
+            closeB.addEventListener("click" , (e)=>{
+                console.log(e) ;
+                v.classList.remove("active") ;
+            })
+            
+            dowm.addEventListener("click" , ()=>{
+    
+            })
